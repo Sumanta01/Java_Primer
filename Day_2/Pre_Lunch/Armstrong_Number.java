3012c5423cb17e3bfc9792cb40cf519d4397e935
Sumanta01 <2170199@kiit.ac.in>
2024-03-06 13:21:30 +0530

    second done

--- Day_2/Pre_Lunch/Armstrong_Number.java
+++ Day_2/Pre_Lunch/Armstrong_Number.java
@@ -1,4 +1,8 @@
 package Day_2.Pre_Lunch;
+/*8. A three digit number is called Armstrong number if sum of cube of its digit is equal to number itself.
+E.g.- 153 is an Armstrong number because (13)+(53)+(33) = 153.
+Write all Armstrong numbers between 100 to 500.
+ */
 
 import java.util.Scanner;
 
