5045040ced2e6e347340ac0e6d5c701d1fb117a1
Sumanta01 <2170199@kiit.ac.in>
2024-03-12 17:05:55 +0530

    updated

--- Day_6/TwoD_Array/src/Array_TwoD.java
+++ Day_6/TwoD_Array/src/Array_TwoD.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Array_TwoD {
     
-
     static boolean isPrime(int n){
         if(n<=1){
             return false;
@@ -13,29 +12,40 @@ public class Array_TwoD {
         }
         return true;
     }
-    static int [][]withoutPrime(int r,int c){
-        int [][]arr=new int[r][c];
-        int count=1;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                while(isPrime(count)){
-                    count++;
-                }
-                arr[i][j]=count++;
-            }
-        }
-        return arr;
-    }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-        int [][]arr=withoutPrime(r,c);
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                System.out.print(arr[i][j]+" ");
+
+        while(true){
+            int r=sc.nextInt();
+            int c=sc.nextInt();
+            if(r>0 && c>0){
+                int [][]arr=new int [r][c];
+                for(int i=0;i<r;i++){
+                    for(int j=0;j<c;j++){
+                        arr[i][j]=sc.nextInt();
+                    }
+                    System.out.println();
+                }
+                
+                for(int i=0;i<r;i++){
+                    for(int j=0;j<c;j++){
+                        if(!isPrime(arr[i][j])){
+                            System.out.print(arr[i][j]+" ");
+
+                        }else{
+                            System.out.print("_ ");
+                        }
+                        
+                    }
+                    System.out.println();
+                    
+                }
+                break;
+            
+            }else{
+                System.out.println("Invalid Input!");
             }
-            System.out.println();
         }
 
 
